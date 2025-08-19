ffbc8b6ed486e89544ffac6b3e40f14f54b74006
ahmed hassan <you@example.com>
2025-08-19 19:59:24 +0500

    not aai

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -30,7 +30,7 @@ const VerifyEmailContent = () => {
         }
 
         const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_BASE_URL || "https://chatsbakend.onrender.com"}/api/user/verify`,
+          `${process.env.NEXT_PUBLIC_API_BASE_URL || 'https://chatsbakend.onrender.com'}/api/verify`,
           { params: { userId, token } }
         );
 
